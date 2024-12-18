82bed8f758d8791d8403c4a8ac277deb227ca716
errommore995@gmail.com <errommore995@gmail.com>
2024-12-18 14:07:33 +0600

    Fix without giga threads

--- CourseLib.cpp
+++ CourseLib.cpp
@@ -40,8 +40,6 @@ void CourseLib::executeRoundRobin(int quantum, std::function<void(const std::str
         outputCallback("Executing Process (Round Robin): ID " + std::to_string(process.id) + " for " +
                        std::to_string(executionTime) + " time units.");
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(executionTime * 100));
-
         process.burstTime -= executionTime;
 
         if (process.burstTime > 0)
@@ -52,8 +50,6 @@ void CourseLib::executeRoundRobin(int quantum, std::function<void(const std::str
         {
             outputCallback("Process ID " + std::to_string(process.id) + " completed.");
         }
-
-        std::this_thread::sleep_for(std::chrono::seconds(2));
     }
 }
 
@@ -67,10 +63,6 @@ void CourseLib::executeFCFS(std::function<void(const std::string &)> outputCallb
         outputCallback("Executing Process (FCFS): ID " + std::to_string(process.id) + " for " +
                        std::to_string(process.burstTime) + " time units.");
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(process.burstTime * 100));
-
         outputCallback("Process ID " + std::to_string(process.id) + " completed.");
-
-        std::this_thread::sleep_for(std::chrono::seconds(2));
     }
 }
